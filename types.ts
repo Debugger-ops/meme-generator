72a43dcbf34af2f37e069979fbe6a4d6a33fd2bb
Debugger-ops <vivek9to5@gmail.com>
2025-03-11 00:49:01 +0530

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,8 @@
+export interface MemeState {
+    image: HTMLImageElement | null;
+    topText: string;
+    bottomText: string;
+    textColor: string;
+    fontSize: number;
+    textStroke: boolean;
+  }
